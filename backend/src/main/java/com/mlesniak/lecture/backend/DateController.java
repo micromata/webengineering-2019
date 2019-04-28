92d3d4e986b343cb4e680780d3c09a336c3a04da
Michael Lesniak <mlesniak@micromata.de>
2019-04-28 12:07:19 +0200

    Add basic controller

---
+++ backend/src/main/java/com/mlesniak/lecture/backend/DateController.java
@@ -0,0 +1,10 @@
+package com.mlesniak.lecture.backend;
+
+/**
+ * This class will allow us to sent HTTP requests to /api/date and return a simple JSON object containing the
+ * current timestamp.
+ *
+ * Question for your own research: Why can't we simply return a string?
+ */
+public class DateController {
+}
