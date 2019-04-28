ccc09e27e207ab077b08b07312a199c18750b288
Michael Lesniak <mlesniak@micromata.de>
2019-04-28 12:08:19 +0200

    Add REST controller
    See https://spring.io/guides/gs/rest-service/ for a basic introduction.

--- backend/src/main/java/com/mlesniak/lecture/backend/DateController.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/DateController.java
@@ -1,10 +1,13 @@
 package com.mlesniak.lecture.backend;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * This class will allow us to sent HTTP requests to /api/date and return a simple JSON object containing the
  * current timestamp.
  *
  * Question for your own research: Why can't we simply return a string?
  */
+@RestController
 public class DateController {
 }
