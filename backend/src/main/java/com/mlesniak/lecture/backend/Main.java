460ee962d8a95d61ae17815b91ea32747f623ef5
Michael Lesniak <mlesniak@micromata.de>
2019-04-28 11:57:35 +0200

    Refactoring
    Note that this is the only time I (usually) deviate from best practices
    for git commits, e.g. see https://chris.beams.io/posts/git-commit/

--- backend/src/main/java/com/mlesniak/lecture/backend/Main.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
