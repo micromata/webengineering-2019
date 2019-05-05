6a6ec45cf9bae0d1e23e418c2776ffe91b00f173
Michael Lesniak <mlesniak@micromata.de>
2019-05-05 14:41:25 +0200

    Show example of a functional component

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,17 @@
+/**
+ * This is a functional component defined by a single returned function. Note that we can't store state in a
+ * functional component.
+ */
+function GreeterPanel(props) {
+    return (
+        <h1>
+            Hello, {props.name}!
+        </h1>
+    )
+}
+
 // See e.g. https://reactjs.org/docs/hello-world.html
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <GreeterPanel name='Lecture'/>,
     document.getElementById('root')
 );
