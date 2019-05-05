c67bfe58b80d14d6f0f1001909f194794be0a4f3
Michael Lesniak <mlesniak@micromata.de>
2019-05-05 16:30:37 +0200

    Return posts sorted by createdAt field

--- backend/src/main/java/com/mlesniak/lecture/backend/PostRepository.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/PostRepository.java
@@ -1,7 +1,11 @@
 package com.mlesniak.lecture.backend;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
-    // Empty for now.
+    @Query("SELECT p FROM Post p ORDER BY p.createdAt DESC")
+    List<Post> findAll();
 }
