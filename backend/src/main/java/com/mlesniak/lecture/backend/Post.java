e2d36bf6622b2b542852633f37ab10d2fdc6cc9f
Michael Lesniak <mlesniak@micromata.de>
2019-05-05 16:24:50 +0200

    Add field for createdAt and initializer

--- backend/src/main/java/com/mlesniak/lecture/backend/Post.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/Post.java
@@ -3,13 +3,19 @@ package com.mlesniak.lecture.backend;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Post {
+    public Post() {
+        createdAt = new Date();
+    }
+
     @Id
     @GeneratedValue
     public Long id;
 
+    public Date createdAt;
     public String title;
     public String content;
 
@@ -17,6 +23,7 @@ public class Post {
     public String toString() {
         return "Post{" +
                 "id=" + id +
+                ", createdAt=" + createdAt +
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
