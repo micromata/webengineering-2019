52c5be34932b3779306c54ea8b169ec07842abc2
Michael Lesniak <mlesniak@micromata.de>
2019-05-05 09:22:54 +0200

    Retrieve all posts

--- backend/src/main/java/com/mlesniak/lecture/backend/PostController.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/PostController.java
@@ -3,10 +3,7 @@ package com.mlesniak.lecture.backend;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
@@ -29,5 +26,11 @@ public class PostController {
         LOG.info("Storing post: {}", post);
         postRepository.save(post);
         return Collections.emptyMap();
+    }
+
+    @GetMapping("/api/post")
+    public Iterable<Post> save() {
+        LOG.info("Retrieving all posts");
+        return postRepository.findAll();
     }
 }
