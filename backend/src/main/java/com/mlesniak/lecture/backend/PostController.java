e61b3a01ea1354324abf3e0fadf36d7b850f292c
Michael Lesniak <mlesniak@micromata.de>
2019-05-05 09:11:13 +0200

    Add (dummy) controller to store post entries

---
+++ backend/src/main/java/com/mlesniak/lecture/backend/PostController.java
@@ -0,0 +1,41 @@
+package com.mlesniak.lecture.backend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Controller to handle all CRUD-related post requests.
+ */
+@CrossOrigin
+@RestController
+public class PostController {
+    private static final Logger LOG = LoggerFactory.getLogger(PostController.class);
+
+    @PostMapping("/api/post")
+    public Map<String, String> save(@RequestBody Post post) {
+        LOG.info("Storing post: {}", post);
+        // TODO ML Store value in database.
+        return Collections.emptyMap();
+    }
+
+    // TODO ML Own model class
+    public static class Post {
+        public String title;
+        public String content;
+
+        @Override
+        public String toString() {
+            return "Post{" +
+                    "title='" + title + '\'' +
+                    ", content='" + content + '\'' +
+                    '}';
+        }
+    }
+}
