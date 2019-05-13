c986118ed2b3519d35d1a1b2b3e9e5dc4b4285fb
Michael Lesniak <mlesniak@micromata.de>
2019-05-13 16:05:44 +0200

    Fix script to create comments for post or comments

--- backend/database-create-random-posts-and-comments.sh
+++ backend/database-create-random-posts-and-comments.sh
@@ -11,8 +11,31 @@ do
         echo "Creating a new post..."
         http POST :8080/api/post title="Title $RANDOM" content="https://en.wikipedia.org/wiki/Special:Random" >/dev/null
     else
-        echo "Creating a comment..."
-        postid=$(http GET :8080/api/debug/all|jq '.[] | .id' | sort -R | head -n 1)
-        http -v POST :8080/api/post/${postid}/comment content="Comment $RANDOM" >/dev/null
+        echo -n "Creating a comment..."
+        n=$(($RANDOM % 100))
+        if [[ ${n} -lt 50 ]]
+        then
+            # Create comment to post for comment
+            postid=$(\
+                http GET :8080/api/debug/all |\
+                jq -r '.[] | (.id|tostring) + " " + .content' |\
+                grep 'Comment' |\
+                sort -R |\
+                head -n 1 |\
+                cut -d' ' -f1)
+            http -v POST :8080/api/comment/${postid}/comment content="Comment/Comment $RANDOM" >/dev/null
+            echo "for a comment"
+        else
+            # Create comment to post for post
+            postid=$(\
+                http GET :8080/api/debug/all |\
+                jq -r '.[] | (.id|tostring) + " " + .content' |\
+                grep -v 'Comment' |\
+                sort -R |\
+                head -n 1 |\
+                cut -d' ' -f1)
+            http -v POST :8080/api/post/${postid}/comment content="Comment/Post $RANDOM" >/dev/null
+            echo "for a post"
+        fi
     fi
 done
