a497d595a8574c906fa14d1d42b4128db2514968
Michael Lesniak <mlesniak@micromata.de>
2019-05-14 10:45:25 +0200

    Change probabilities

--- backend/database-create-random-posts-and-comments.sh
+++ backend/database-create-random-posts-and-comments.sh
@@ -6,35 +6,34 @@ for i in $(seq 1 ${runs})
 do
     echo -n "RUN $i: "
     n=$(($RANDOM % 100))
+    # TODO ML Add variables for easier configuration.
     if [[ ${n} -lt 10 ]]
     then
         echo "Creating a new post..."
-        http POST :8080/api/post title="Title $RANDOM" content="https://en.wikipedia.org/wiki/Special:Random" >/dev/null
+        http POST :8080/api/post title="Title $RANDOM" url="https://en.wikipedia.org/wiki/Special:Random" >/dev/null
     else
         echo -n "Creating a comment..."
         n=$(($RANDOM % 100))
-        if [[ ${n} -lt 50 ]]
+        if [[ ${n} -lt 30 ]]
         then
             # Create comment to post for comment
             postid=$(\
                 http GET :8080/api/debug/all |\
-                jq -r '.[] | (.id|tostring) + " " + .content' |\
-                grep 'Comment' |\
+                jq -r '.[] | select (.url == null) | .id' |\
                 sort -R |\
                 head -n 1 |\
                 cut -d' ' -f1)
-            http -v POST :8080/api/comment/${postid}/comment content="Comment/Comment $RANDOM" >/dev/null
+            http -v POST :8080/api/comment/${postid}/comment comment="Comment/Comment $RANDOM" >/dev/null
             echo "for a comment"
         else
             # Create comment to post for post
             postid=$(\
                 http GET :8080/api/debug/all |\
-                jq -r '.[] | (.id|tostring) + " " + .content' |\
-                grep -v 'Comment' |\
+                jq -r '.[] | select (.url != null) | .id' |\
                 sort -R |\
                 head -n 1 |\
                 cut -d' ' -f1)
-            http -v POST :8080/api/post/${postid}/comment content="Comment/Post $RANDOM" >/dev/null
+            http -v POST :8080/api/post/${postid}/comment comment="Comment/Post $RANDOM" >/dev/null
             echo "for a post"
         fi
     fi
