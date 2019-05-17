64987003d8f340570ec0e3c701d3b35986affc7e
Michael Lesniak <mlesniak@micromata.de>
2019-05-17 13:38:43 +0200

    Add ordering of post lists

--- backend/src/main/java/com/mlesniak/lecture/backend/PostRepository.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/PostRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-    @Query(value = "SELECT p FROM Post p WHERE p.title IS NOT NULL")
+    @Query(value = "SELECT p FROM Post p WHERE p.title IS NOT NULL ORDER BY size(p.comments) DESC, p.createdAt DESC")
     List<Post> findPosts();
-
-//    @Query(value = "SELECT p FROM Post p WHERE p.parent.id = :id")
-//    List<Post> findComments(@Param("id") long id);
 }
