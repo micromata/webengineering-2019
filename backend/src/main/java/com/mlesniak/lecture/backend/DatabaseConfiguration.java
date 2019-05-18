32be56ee1bf1b1479770c4d45a663ebb716bf27b
Michael Lesniak <mlesniak@micromata.de>
2019-05-18 20:30:29 +0200

    Add database configuration for heroku

---
+++ backend/src/main/java/com/mlesniak/lecture/backend/DatabaseConfiguration.java
@@ -0,0 +1,33 @@
+package com.mlesniak.lecture.backend;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Enable configuration over a single environment variable, e.g. as done in heroku.
+ * <p>
+ * See https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-java.
+ */
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
