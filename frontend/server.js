9aa0566f43bc042c62df6e848ae9d0c81e689f91
Michael Lesniak <mlesniak@micromata.de>
2019-05-31 18:16:24 +0200

    Use dist directory

--- frontend/server.js
+++ frontend/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 // Serve all static files from here.
-app.use(express.static('.'));
+app.use(express.static('./dist'));
 
 // Serve all other URLs by returning the main page.
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 // Start listening.
