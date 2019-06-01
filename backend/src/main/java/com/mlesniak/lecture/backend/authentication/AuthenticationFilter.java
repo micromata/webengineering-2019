0f4c1f1a3a30bbe44ae6e9367d33130ca04afcfa
Michael Lesniak <mlesniak@micromata.de>
2019-06-01 19:07:28 +0200

    Check for existence of authorization header

--- backend/src/main/java/com/mlesniak/lecture/backend/authentication/AuthenticationFilter.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/authentication/AuthenticationFilter.java
@@ -1,23 +1,37 @@
 package com.mlesniak.lecture.backend.authentication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 public class AuthenticationFilter implements Filter {
+    private static final Logger LOG = LoggerFactory.getLogger(AuthenticationFilter.class);
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest) req;
+        HttpServletResponse httpResp = (HttpServletResponse) resp;
 
         // In our case we do not permit any HTTP requests which are POST.
-        HttpServletRequest httpReq = (HttpServletRequest) req;
         if (httpReq.getMethod().equals("POST")) {
-            System.out.println("POST");
+            String authorization = httpReq.getHeader("Authorization");
+            if (StringUtils.isEmpty(authorization)) {
+                LOG.warn("Unauthorized request to {}", ((HttpServletRequest) req).getRequestURI());
+                ((HttpServletResponse) resp).setStatus(HttpStatus.UNAUTHORIZED.value());
+                return;
+            }
         }
 
+        // We allow everything else.
         chain.doFilter(req, resp);
     }
 }
