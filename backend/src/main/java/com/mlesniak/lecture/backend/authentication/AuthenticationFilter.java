7a6875ce3ab2baaa4fff16bc15915edff5280474
Michael Lesniak <mlesniak@micromata.de>
2019-06-01 18:59:26 +0200

    Add check for HTTP POST

--- backend/src/main/java/com/mlesniak/lecture/backend/authentication/AuthenticationFilter.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/authentication/AuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.mlesniak.lecture.backend.authentication;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Component
@@ -10,6 +11,13 @@ public class AuthenticationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
             throws IOException, ServletException {
+
+        // In our case we do not permit any HTTP requests which are POST.
+        HttpServletRequest httpReq = (HttpServletRequest) req;
+        if (httpReq.getMethod().equals("POST")) {
+            System.out.println("POST");
+        }
+
         chain.doFilter(req, resp);
     }
 }
