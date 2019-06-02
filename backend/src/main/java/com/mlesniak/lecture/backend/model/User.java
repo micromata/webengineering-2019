36a0a93843131fa621030d88fc6e90446e14c7c6
Michael Lesniak <mlesniak@micromata.de>
2019-06-02 06:28:51 +0200

    Fix user object (postgres and future injection)

--- backend/src/main/java/com/mlesniak/lecture/backend/model/User.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/model/User.java
@@ -5,16 +5,43 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
+@Table(name = "users") // Postgress does not allow the reserved name user. Thanks to docker, we saw it directly!
 public class User {
     @Id
     @GeneratedValue
     @JsonProperty(access = JsonProperty.Access.READ_ONLY) // See JavaDoc for explanation.
     public Long id;
 
-    public String userName;
-    public String fullName;
+    String userName;
+    String fullName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
     @Override
     public String toString() {
