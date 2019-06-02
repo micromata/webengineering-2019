91a7a2c0d2e40911b88932f585958c1a78a98a3e
Michael Lesniak <mlesniak@micromata.de>
2019-06-02 06:05:56 +0200

    Add basic check for valid JWT token

--- backend/src/main/java/com/mlesniak/lecture/backend/authentication/AuthenticationFilter.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/authentication/AuthenticationFilter.java
@@ -1,15 +1,23 @@
 package com.mlesniak.lecture.backend.authentication;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.impl.DefaultClaims;
+import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
+import javax.crypto.SecretKey;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
+import java.util.Optional;
 
 @Component
 public class AuthenticationFilter implements Filter {
@@ -23,7 +31,7 @@ public class AuthenticationFilter implements Filter {
 
         // In our case we do not permit any HTTP requests which are POST.
         if (httpReq.getMethod().equals("POST")) {
-            if (isAuthorized(httpReq)) {
+            if (!isAuthorized(httpReq)) {
                 LOG.warn("Unauthorized request to {}", ((HttpServletRequest) req).getRequestURI());
                 httpResp.setStatus(HttpStatus.UNAUTHORIZED.value());
                 return;
@@ -35,7 +43,32 @@ public class AuthenticationFilter implements Filter {
     }
 
     private boolean isAuthorized(HttpServletRequest request) {
+        // TODO ML Add user to context.
+        Optional<Claims> claims = decodeRequest(request);
+        LOG.info("Claims: {}", claims);
+        return claims.isPresent();
+    }
+
+    public Optional<Claims> decodeRequest(HttpServletRequest request) {
+        // TODO ML Use environment variable for key.
+        // Base64 encoded qwertyuiopasdfghjklzxcvbnm123456
+        String secretKey = "cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0xMjM0NTY=";
+
+        // Use header information from header 'Authorization' to extract user from JWT.
         String authorization = request.getHeader("Authorization");
-        return !StringUtils.isEmpty(authorization);
+
+        int minStringLength = "Bearer ".length();
+        if (authorization == null || authorization.length() < minStringLength) {
+            return Optional.empty();
+        }
+        authorization = authorization.substring(minStringLength);
+        SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secretKey.getBytes()));
+        try {
+            Jwt jwt = Jwts.parser().setSigningKey(key).parseClaimsJws(authorization);
+            return Optional.of(((DefaultClaims) jwt.getBody()));
+        } catch (JwtException e) {
+            LOG.warn("Malformed JWT token submitted: <{}>", authorization);
+            return Optional.empty();
+        }
     }
 }
