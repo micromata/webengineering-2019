b2174fae6979a1e915e93799f05f8edc51cbad81
Michael Lesniak <mlesniak@micromata.de>
2019-06-02 05:45:11 +0200

    Fix token

--- backend/src/main/java/com/mlesniak/lecture/backend/authentication/AuthenticationController.java
+++ backend/src/main/java/com/mlesniak/lecture/backend/authentication/AuthenticationController.java
@@ -16,7 +16,7 @@ public class AuthenticationController {
     public Map<String, String> getUserInfor(@RequestParam("code") String code) {
         Map<String, String> map = new HashMap<>();
         // Create an actual JWT token, e.g. see http://jwtbuilder.jamiekurtz.com/ for a generator online.
-        map.put("token", "yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
+        map.put("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE1NTk0NDYzODksImV4cCI6MTU5MDk4MjM4OSwiYXVkIjoiIiwic3ViIjoibWxlc25pYWsiLCJuYW1lIjoiRHIuIE1pY2hhZWwgTGVzbmlhayIsImlkIjoiMSJ9.T9rexyIBnGGjqEvZtyV1E0EPbWdzgNc-X1_qHo3cCts");
         return map;
     }
 }
