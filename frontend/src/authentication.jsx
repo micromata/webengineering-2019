1644d348b259b6a044b8b94ff1604211f33bb27f
Michael Lesniak <mlesniak@micromata.de>
2019-06-16 17:14:31 +0200

    Create singleton inside a class

--- frontend/src/authentication.jsx
+++ frontend/src/authentication.jsx
@@ -1,89 +1,97 @@
-// TODO ML Create class for this set of methods for easier import and more structure.
-// TODO ML Create singleton for this class using e.g. http://www.adam-bien.com/roller/abien/entry/singleton_pattern_in_es6_and
-// TODO ML Refactor this file!
 import React from "react";
 import jwt_decode from 'jwt-decode';
 import backend from './configuration';
 
+export class Authentication {
+    constructor() {
+        this.listener = [];
+        this.loadStoredToken();
 
-export function loadStoredToken() {
-    let token = getCookie("token");
-    window.jwt = token;
-    if (token !== "") {
+        this.logout = this.logout.bind(this);
+    }
+
+    loadStoredToken() {
+        let token = this.getCookie("token");
+        window.jwt = token;
+        if (token !== "") {
+            const decoded = jwt_decode(token);
+            window.token = decoded;
+        }
+    }
+
+    isAuthenticated() {
+        return window.token != undefined;
+    }
+
+    getToken() {
+        return window.jwt;
+    }
+
+    getUser() {
+        return window.token;
+    }
+
+    authenticate(token) {
+        this.setCookie("token", token, 90);
         const decoded = jwt_decode(token);
         window.token = decoded;
+        window.jwt = token;
+        // TODO ML Rename this function
+        this.listener.forEach(l => l.authenticated());
+    }
+
+    addAuthenticationListener(l) {
+        this.listener.push(l);
+    }
+
+    logout() {
+        window.token = null;
+        this.eraseCookie("token");
+        this.listener.forEach(l => l.authenticated());
+
+        // We are logged out, go back to the main page.
+        document.location.href = "/";
+    }
+
+    setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    eraseCookie(name) {
+        document.cookie = name + '=; Max-Age=-99999999;';
+    }
+
+    getAuthenticationURL(callback) {
+        fetch(backend + '/api/authentication/url')
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                callback(data.url);
+            })
     }
 }
 
-export function isAuthenticated() {
-    return window.token != undefined;
-}
+const instance = new Authentication();
+Object.freeze(instance);
 
-export function getToken() {
-    return window.jwt;
-}
-
-export function getUser() {
-    return window.token;
-}
-
-export function authenticate(token) {
-    setCookie("token", token, 90);
-    const decoded = jwt_decode(token);
-    window.token = decoded;
-    window.jwt = token;
-    // TODO ML Rename this function
-    listener.forEach(l => l.authenticated());
-}
-
-let listener = [];
-
-export function addAuthenticationListener(l) {
-    listener.push(l);
-}
-
-export function logout() {
-    window.token = null;
-    eraseCookie("token");
-    listener.forEach(l => l.authenticated());
-
-    // We are logged out, go back to the main page.
-    document.location.href = "/";
-}
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function eraseCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
-}
-
-export function getAuthenticationURL(callback) {
-    fetch(backend + '/api/authentication/url')
-        .then((response) => {
-            return response.json()
-        })
-        .then((data) => {
-            callback(data.url);
-        })
-}
+export default instance;
