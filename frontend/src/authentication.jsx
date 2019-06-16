b9968e4238e8e293c3120465a6630ce232308c81
Michael Lesniak <mlesniak@micromata.de>
2019-06-16 17:24:14 +0200

    Replace var with let

--- frontend/src/authentication.jsx
+++ frontend/src/authentication.jsx
@@ -56,18 +56,18 @@ export class Authentication {
     }
 
     setCookie(cname, cvalue, exdays) {
-        var d = new Date();
+        let d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires=" + d.toUTCString();
+        let expires = "expires=" + d.toUTCString();
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 
     getCookie(cname) {
-        var name = cname + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
             while (c.charAt(0) == ' ') {
                 c = c.substring(1);
             }
